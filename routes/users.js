a2adf8d1a88f5fe9636c33225b6683216d567b69
Rizaldi Farhan <rizaldifarhan28@gmail.com>
2023-12-13 22:08:45 +0700

    Intialize routes users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var userControllers = require('../controllers/userControllers'); // Correct the file path
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+
+// GET all users
+router.get('/', userControllers.getAllUsers);
+
+// GET user detail
+router.get('/:id', userControllers.getUserDetail);
+
+// POST create a new user (Sign up)
+router.post('/', userControllers.signUp);
+
+// POST user sign in
+router.post('/signin', userControllers.signIn);
+
+// POST user sign out
+router.post('/signout', userControllers.signOut);
+
+// POST user sing up
+router.post('/signup', userControllers.signUp);
+
+// PUT update user
+router.put('/:id', userControllers.updateUser);
+
+// DELETE delete user
+router.delete('/:id', userControllers.deleteUser);
 
 module.exports = router;

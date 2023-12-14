0bb28b016fce899227260c3de41f4c3d11cb0047
Rizaldi Farhan <rizaldifarhan28@gmail.com>
2023-12-14 12:51:18 +0700

    Reconfigure User Routers

--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,29 @@
-var express = require('express');
-var router = express.Router();
-var userControllers = require('../controllers/userControllers'); // Correct the file path
+  var express = require('express');
+  var router = express.Router();
+  var userControllers = require('../controllers/userControllers'); // Correct the file path
+  var { authenticateToken, authenticateRefreshToken, checkBlacklist , isAdmin,isUserOwner  } = require('../middlewares/auth');
 
 
 
-// GET all users
-router.get('/', userControllers.getAllUsers);
+  // GET all users
+  router.get('/', authenticateToken, authenticateRefreshToken, isAdmin, checkBlacklist, checkBlacklist, userControllers.getAllUsers);
 
-// GET user detail
-router.get('/:id', userControllers.getUserDetail);
+  // GET user detail
+  router.get('/:id', authenticateToken, authenticateRefreshToken, isUserOwner, checkBlacklist, userControllers.getUserDetail);
+  // POST user sign in
+  router.post('/signin', userControllers.signIn);
 
-// POST create a new user (Sign up)
-router.post('/', userControllers.signUp);
+  // POST user sign out
+  router.post('/signout',  authenticateToken, authenticateRefreshToken, checkBlacklist , checkBlacklist, userControllers.signOut);
 
-// POST user sign in
-router.post('/signin', userControllers.signIn);
+  // POST user sign up
+  router.post('/signup', userControllers.signUp);
 
-// POST user sign out
-router.post('/signout', userControllers.signOut);
 
-// POST user sing up
-router.post('/signup', userControllers.signUp);
+  // PUT update user
+  router.put('/:id', authenticateToken, authenticateRefreshToken, isUserOwner, checkBlacklist, userControllers.updateUser);
 
-// PUT update user
-router.put('/:id', userControllers.updateUser);
+  // DELETE delete user
+  router.delete('/:id', authenticateToken, authenticateRefreshToken, isAdmin, checkBlacklist, userControllers.deleteUser);
 
-// DELETE delete user
-router.delete('/:id', userControllers.deleteUser);
-
-module.exports = router;
+  module.exports = router;
